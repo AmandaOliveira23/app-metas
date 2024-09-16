010f74f0ea543898713b89bc28ab08e2b05d79d8
Amanda_Oliveira <amandacris.oliveiraa@gmail.com>
2024-09-16 01:13:33 -0300

    cadastrar e listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {select, input} = require('@inquirer/prompts')
+const {select, input, checkbox} = require('@inquirer/prompts')
 
 
 const meta = {
@@ -17,10 +17,38 @@ const cadastrarMeta = async () =>{
     }
 
     metas.push({
-        value: metas,
+        value: meta,
         checked: "false"
     })
 
+}
+
+const listarMetas = async () =>{
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices:[...metas],
+        instructions: false,
+    })
+
+    if(respostas.lenght == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    
+    metas.forEach((m) =>{
+        m.checked = false
+    })
+
+    respostas.forEach((resposta)=>{
+        const meta = metas.find((m) =>{
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
 }
 
 const start = async () =>{
@@ -55,6 +83,8 @@ const start = async () =>{
                 console.log("vamos cadastrar")
                 break
             case "listar":
+                await listarMetas()
+                
                 console.log("vamos listar")
                 break
             case "sair":
